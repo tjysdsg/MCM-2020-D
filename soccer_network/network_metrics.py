ab4de326a801c2358375653f59a1777507096f8d
tjysdsg <tjy1018543509@163.com>
2020-02-16 12:32:11 +0800

    Update motifs

--- soccer_network/network_metrics.py
+++ soccer_network/network_metrics.py
@@ -17,7 +17,7 @@ topology
 """
 
 
-def metrics_passing_volume(g: Graph):
+def passing_volume(g: Graph):
     vs = g.get_vertices()
     total = g.get_in_degrees(vs) + g.get_out_degrees(vs)
     return np.mean(total), np.std(total)
@@ -34,37 +34,47 @@ def post_passing_volume(results: Tuple):
     print(df.corr())
 
 
-def metric_motifs(g: Graph):
-    return clustering.motif_significance(g, 3)
+def motifs(g: Graph, k: int = 3):
+    return clustering.motif_significance(g, k)
 
 
-def post_motifs(results: Tuple):
-    z_scores = results[1]
-    print(z_scores)
+def post_motifs(results: List[Tuple]):
+    sorted_zs = []
+    for mn, z in results:
+        z = np.asarray(z)
+        z[::-1].sort()
+        sorted_zs.append(z)
+    print(sorted_zs)
+    return sorted_zs
 
 
 # TODO: add your metrics here, the function should have only one required argument and can return anything you like
 metrics = [
-    metrics_passing_volume,
-    metric_motifs
+    passing_volume,
+    motifs
 ]
 
 # TODO| add your post-metrics-computation processing function here,
 # the results of every metric **for all graphs** are passed in as **one** single argument
 # NOTE: the order of results is the same as the order of `match_ids`
-post_metrics = [post_passing_volume]
+post_metrics = [post_passing_volume, post_motifs]
 
 
 def run_metric(gs: List[Graph], metric: Callable[[Graph], Any], post_metric: Callable):
+    print('Running {0}'.format(metric.__name__))
     results = [metric(g) for g in gs]
+    print('Running {0}'.format(post_metric.__name__))
     post_metric(results)
 
 
 if __name__ == "__main__":
+    print('Loading graphml files...')
     graphs = [load_graphml('../graphs/network-{0}.xml'.format(mi)) for mi in match_ids]
     # graphs = [load_graphml('../graphs/zoned-network-{0}.xml'.format(mi)) for mi in match_ids]
+
+    print('Calculating metrics...')
     # run a single metric
-    run_metric(graphs, metrics_passing_volume, post_passing_volume)
+    run_metric(graphs, motifs, post_motifs)
 
     """
     # or run all metrics
